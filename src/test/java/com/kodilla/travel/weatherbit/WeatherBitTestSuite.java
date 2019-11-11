af70afe2d030e08b1a047f51a8c1d38b993196a4
MAcewicz <acewicz.m@gmail.com>
2019-11-11 07:39:40 +0000

    fixed repo

--- src/test/java/com/kodilla/travel/weatherbit/WeatherBitTestSuite.java
+++ src/test/java/com/kodilla/travel/weatherbit/WeatherBitTestSuite.java
@@ -1,17 +1,38 @@
 package com.kodilla.travel.weatherbit;
 
-import com.kodilla.travel.domain.Weather;
+import com.kodilla.travel.dto.ForecastDto;
+import com.kodilla.travel.dto.WeatherDto;
+import com.kodilla.travel.exception.AirportNotFoundException;
+import com.kodilla.travel.mappers.WeatherMapper;
+import com.kodilla.travel.service.AirportService;
+import com.kodilla.travel.service.WeatherService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WeatherBitTestSuite {
 
+    @Autowired
+    private WeatherbitClient weatherbitClient;
+
+    @Autowired
+    private AirportService airportService;
+
     @Test
-    public void testGetLongForecast() {
+    public void testGetLongForecast() throws AirportNotFoundException {
+        ForecastDto forecastDto = weatherbitClient.getForecast(airportService.getAirportByIATA("PRG")
+                .orElseThrow(AirportNotFoundException::new));
+
+        List<WeatherDto> weatherDtoList = forecastDto.getWeatherDtos();
+
+        for (WeatherDto weatherDto : weatherDtoList) {
+            System.out.println(weatherDto);
+        }
     }
 }
