3b0fe123348c35fcb8886f13e812286a7c230e1d
MAcewicz <acewicz.m@gmail.com>
2022-10-16 17:42:29 +0200

    migrated from restTemplate to webClient

--- src/main/java/com/kodilla/travel/config/CoreConfiguration.java
+++ src/main/java/com/kodilla/travel/config/CoreConfiguration.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @EnableScheduling
 public class CoreConfiguration {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient() {
+        return WebClient.builder().build();
     }
 }
--- src/main/java/com/kodilla/travel/weatherbit/WeatherbitClient.java
+++ src/main/java/com/kodilla/travel/weatherbit/WeatherbitClient.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -16,11 +17,12 @@ public class WeatherbitClient {
     @Value("${weatherbit.key}")
     private String key;
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
+
 
     @Autowired
-    public WeatherbitClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public WeatherbitClient(WebClient webClient) {
+        this.webClient = webClient;
     }
 
     private String getBaseUrl() {
@@ -36,6 +38,10 @@ public class WeatherbitClient {
                 .encode()
                 .toUri();
 
-        return restTemplate.getForObject(url, ForecastDto.class);
+        return webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(ForecastDto.class)
+                .block();
     }
 }
